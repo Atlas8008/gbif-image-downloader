d9b97d546daa79b29c0eed1fc994b5b5f88db95d
koerschens <matthias.koerschens@uni-jena.de>
2025-02-18 14:30:59 +0100

    Added nomatch option to lookup

--- species_lookup.py
+++ species_lookup.py
@@ -5,6 +5,7 @@ import requests
 parser = argparse.ArgumentParser(description="Looks up a list of species names in the GBIF database to check what actual name or synonym would be found when searching for a specific term or name.")
 
 parser.add_argument("list_file", type=str, help="The file containing the list of species to look up.")
+parser.add_argument("--nomatch_only", action="store_true", help="Flag; if set, only the GBIF names will be printed that do not contain the query name.")
 
 args = parser.parse_args()
 
@@ -21,5 +22,6 @@ for species_name in species_names:
 
     response = json.loads(response.content)
 
-    print("Found:", response["scientificName"])
-    print()
+    if not args.nomatch_only or species_name not in response["scientificName"]:
+        print("Found:", response["scientificName"])
+        print()
